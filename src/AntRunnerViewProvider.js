9efa38bdba709cc18686e7d63938d345533efc51
NickHeap <nickheap@gmail.com>
2018-04-15 17:39:07 +0100

    Add better dependency handling
    Recursively cascade dependencies in tree.
    Allow dependencies to be run.
    Highlight the default target.

--- src/AntRunnerViewProvider.js
+++ src/AntRunnerViewProvider.js
@@ -2,6 +2,7 @@ const vscode = require('vscode')
 const fs = require('fs')
 const path = require('path')
 const xml2js = require('xml2js')
+const _ = require('lodash')
 
 var antTerminal
 var configOptions
@@ -9,6 +10,8 @@ var antHome
 // var envVarsSettings
 var antExecutable
 var sortTargetsAlphabetically
+
+var project
 
 module.exports = class AntRunnerViewProvider {
   constructor (context) {
@@ -34,10 +37,11 @@ module.exports = class AntRunnerViewProvider {
     this.onDidChangeTreeData = this._onDidChangeTreeData.event
 
     this.getConfigOptions()
-    vscode.workspace.onDidChangeConfiguration(() => {
-      this.getConfigOptions()
-      this.refresh()
-    })
+  }
+
+  onDidChangeConfiguration () {
+    this.getConfigOptions()
+    this.refresh()
   }
 
   getConfigOptions () {
@@ -53,7 +57,64 @@ module.exports = class AntRunnerViewProvider {
   }
 
   getTreeItem (element) {
-    return element
+    if (element.contextValue === 'antFile') {
+      return {
+        id: element.filePath,
+        contextValue: element.contextValue,
+        label: element.fileName,
+        command: '',
+        collapsibleState: vscode.TreeItemCollapsibleState.Expanded,
+        tooltip: element.filePath
+      }
+    } else if (element.contextValue === 'antTarget') {
+      let treeItem = {
+        id: element.name,
+        label: element.name,
+        command: 'vscode-ant.runAntTask',
+        arguments: [element.name],
+        contextValue: 'antTarget',
+        tooltip: element.description
+      }
+      // can be expanded for depends?
+      if (element.depends) {
+        treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed
+      } else {
+        treeItem.collapsibleState = vscode.TreeItemCollapsibleState.None
+      }
+      if (element.name === project.$.default) {
+        treeItem.iconPath = {
+          light: path.join(__filename, '..', '..', 'resources', 'icons', 'light', 'default.svg'),
+          dark: path.join(__filename, '..', '..', 'resources', 'icons', 'dark', 'default.svg')
+        }
+      } else {
+        treeItem.iconPath = {
+          light: path.join(__filename, '..', '..', 'resources', 'icons', 'light', 'target.svg'),
+          dark: path.join(__filename, '..', '..', 'resources', 'icons', 'dark', 'target.svg')
+        }
+      }
+
+      return treeItem
+    } else if (element.contextValue === 'antDepends') {
+      let treeItem = {
+        label: element.name,
+        command: 'vscode-ant.runAntDependency',
+        arguments: [element.name],
+        contextValue: 'antDepends',
+        iconPath: {
+          light: path.join(__filename, '..', '..', 'resources', 'icons', 'light', 'dependency.svg'),
+          dark: path.join(__filename, '..', '..', 'resources', 'icons', 'dark', 'dependency.svg')
+        }
+      }
+      // can be expanded for depends?
+      if (element.depends) {
+        treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed
+      } else {
+        treeItem.collapsibleState = vscode.TreeItemCollapsibleState.None
+      }
+      return treeItem
+    } else {
+      return element
+    }
   }
 
   getChildren (element) {
@@ -64,26 +125,53 @@ module.exports = class AntRunnerViewProvider {
         reject(new Error('Failed somehow'))
       })
     }
-
     return new Promise((resolve, reject) => {
-      if (element) {
-        resolve([])
-        reject(new Error('It is empty!'))
-      } else {
-        const buildXml = path.join(this.rootPath, 'build.xml')
+      // add root element?
+      if (!element) {
+        var buildXml = path.join(this.rootPath, 'build.xml')
         if (this.pathExists(buildXml)) {
-          this.getTargetsInBuildXml(buildXml)
+          var root = {
+            id: 'build.xml',
+            contextValue: 'antFile',
+            filePath: buildXml,
+            fileName: 'build.xml'
+          }
+          resolve([root])
+        } else {
+          vscode.window.showInformationMessage('Workspace has no build.xml.')
+          resolve([])
+        }
+      } else {
+        if (element.contextValue === 'antFile' && element.filePath) {
+          this.getTargetsInBuildXml(element.filePath)
             .then((targets) => {
               resolve(targets)
             })
             .catch((err) => {
-              // reject(new Error(err))
+              console.log(err)
+              resolve([])
+            })
+        } else if (element.contextValue === 'antTarget' && element.depends) {
+          this.getDependsInTarget(element)
+            .then((depends) => {
+              resolve(depends)
+            })
+            .catch((err) => {
+              console.log(err)
+              resolve([])
+            })
+        } else if (element.contextValue === 'antDepends' && element.depends) {
+          this.getDependsInTarget(element)
+            .then((depends) => {
+              resolve(depends)
+            })
+            .catch((err) => {
               console.log(err)
               resolve([])
             })
         } else {
-          vscode.window.showInformationMessage('Workspace has no build.xml.')
           resolve([])
+          reject(new Error('Something went wrong!'))
         }
       }
     })
@@ -100,24 +188,58 @@ module.exports = class AntRunnerViewProvider {
           if (err) {
             vscode.window.showInformationMessage('Error parsing build.xml!')
             reject(new Error('Error parsing build.xml!:' + err))
+          } else {
+            vscode.window.showInformationMessage('Targets loaded from build.xml!')
+
+            project = this.setParentValues(result.project)
+            var targets = project.target.map((target) => {
+              var antTarget = {
+                id: target.$.name,
+                contextValue: 'antTarget',
+                depends: target.$.depends,
+                name: target.$.name
+              }
+              return antTarget
+            })
+
+            resolve(this._sort(targets))
           }
-          vscode.window.showInformationMessage('Targets loaded from build.xml!')
-
-          var targets = result.project.target.map((target) => {
-            return {
-              label: target.$.name,
-              collapsibleState: vscode.TreeItemCollapsibleState.None,
-              command: 'vscode-ant.runAntTask',
-              contextValue: 'antTarget',
-              iconPath: false,
-              depends: target.$.depends,
-              targetName: target.$.name
-            }
-          })
-
-          resolve(this._sort(targets))
         })
       })
+    })
+  }
+
+  setParentValues (o) {
+    if (o.target) {
+      for (let n in o.target) {
+        o.target[n].parent = o
+        this.setParentValues(o.target[n])
+      }
+    }
+    return o
+  }
+
+  getDependsInTarget (element) {
+    return new Promise((resolve, reject) => {
+      var depends = element.depends.split(',').map((depends) => {
+        var dependsTarget = {
+          id: depends,
+          contextValue: 'antDepends',
+          name: depends
+        }
+        // get details of this target
+        var target = _.find(project.target, (o) => {
+          if (o.$.name === depends && o.parent.target) {
+            return true
+          }
+          return false
+        })
+        if (target) {
+          dependsTarget.depends = target.$.depends
+        }
+        return dependsTarget
+      })
+      resolve(depends)
     })
   }
 
@@ -131,12 +253,16 @@ module.exports = class AntRunnerViewProvider {
     return true
   }
 
+  runSelectedAntTarget (context) {
+    console.log('wut?')
+  }
+
   runAntTarget (context) {
     if (!context) {
       return
     }
 
-    var target = context.targetName
+    var target = context.name
 
     if (!antTerminal) {
       var envVars = {}
@@ -157,9 +283,9 @@ module.exports = class AntRunnerViewProvider {
     }
 
     return nodes.sort((n1, n2) => {
-      if (n1.label < n2.label) {
+      if (n1.name < n2.name) {
         return -1
-      } else if (n1.label > n2.label) {
+      } else if (n1.name > n2.name) {
         return 1
       } else {
         return 0
@@ -168,7 +294,7 @@ module.exports = class AntRunnerViewProvider {
   }
 
   terminalClosed (terminal) {
-    if (terminal._id === antTerminal._id) {
+    if (terminal.name === antTerminal.name) {
       antTerminal = null
     }
   }
--- src/extension.js
+++ src/extension.js
@@ -6,11 +6,24 @@ const AntRunnerViewProvider = require('./AntRunnerViewProvider')
 function activate (context) {
   const antRunnerViewProvider = new AntRunnerViewProvider(context)
 
-  var antRunnerView = vscode.window.registerTreeDataProvider('antRunnerView', antRunnerViewProvider)
+  var antRunnerView = vscode.window.createTreeView('antRunnerView', {treeDataProvider: antRunnerViewProvider})
+  // antRunnerView = vscode.window.registerTreeDataProvider('antRunnerView', antRunnerViewProvider)
   context.subscriptions.push(antRunnerView)
 
   var runAntTarget = vscode.commands.registerCommand('vscode-ant.runAntTarget', antRunnerViewProvider.runAntTarget)
   context.subscriptions.push(runAntTarget)
+
+  var runAntDependency = vscode.commands.registerCommand('vscode-ant.runAntDependency', antRunnerViewProvider.runAntTarget)
+  context.subscriptions.push(runAntDependency)
+
+  var runSelectedAntTarget = vscode.commands.registerCommand('vscode-ant.runSelectedAntTarget', antRunnerViewProvider.runSelectedAntTarget)
+  context.subscriptions.push(runSelectedAntTarget)
+
+  var refreshAntTargets = vscode.commands.registerCommand('vscode-ant.refreshAntTargets', () => antRunnerViewProvider.refresh())
+  context.subscriptions.push(refreshAntTargets)
+
+  var onDidChangeConfiguration = vscode.workspace.onDidChangeConfiguration(() => antRunnerViewProvider.onDidChangeConfiguration())
+  context.subscriptions.push(onDidChangeConfiguration)
 
   var terminalClosed = vscode.window.onDidCloseTerminal(antRunnerViewProvider.terminalClosed)
   context.subscriptions.push(terminalClosed)
